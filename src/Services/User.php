975d50e9e72313f9ad7326b6f7919522c5f63c69
Julien Sartory <jsartory@gmail.com>
2024-08-26 17:36:05 +0200

    feat: user role config

--- src/Services/User.php
+++ src/Services/User.php
@@ -2,6 +2,8 @@
 
 namespace Daerisimber\Services;
 
+use Daerisimber\Config;
+
 class User
 {
     public function __construct()
@@ -9,8 +11,6 @@ class User
         //Here we can customize
         add_action('init', [$this,'set_roles_capabilities']);
 
-        //Clients like to be Administrator
-        add_action('init', [$this,'change_roles_name']);
     }
     /**
      * Customize role capabilities
@@ -20,20 +20,35 @@ class User
      */
     public function set_roles_capabilities(): void
     {
-        $editor = get_role('editor');
-        $editor->add_cap('edit_themes');
-
-    }
-
-    public function change_roles_name()
-    {
         global $wp_roles;
         if (! isset($wp_roles)) {
             $wp_roles = new \WP_Roles();
         }
-        $wp_roles->roles['administrator']['name'] = 'Full admin';
-        $wp_roles->role_names['administrator'] = 'Full admin';
-        $wp_roles->roles['editor']['name'] = 'Administrator';
-        $wp_roles->role_names['editor'] = 'Administrator';
+
+        $data = Config::get('user.roles');
+        foreach ($data as $key => $value) {
+            $role = get_role($key);
+            if (isset($value['capabilities']['add'])) {
+                foreach ($value['capabilities']['add'] as $cap) {
+                    if (!$role->has_cap($cap)) {
+                        $role->add_cap($cap);
+                    }
+                }
+            }
+            if (isset($value['capabilities']['remove'])) {
+                foreach ($value['capabilities']['remove'] as $cap) {
+                    if ($role->has_cap($cap)) {
+                        $role->remove_cap($cap);
+                    }
+                }
+            }
+
+            if(isset($value['name'])) {
+                $wp_roles->role_names[$key] = $value['name'];
+                $wp_roles->roles[$key]['name'] = $value['name'];
+            }
+        }
+
     }
+
 }
