03d4dd64f832bc7f7126b994c2f5a8607bb433a8
Julien Sartory <jsartory@gmail.com>
2024-08-26 18:14:40 +0200

    fix: message if ACF is missing

--- src/Boot.php
+++ src/Boot.php
@@ -3,18 +3,28 @@
 namespace Daerisimber;
 
 use Timber\Timber;
-use Daerisimber\Config;
 
 class Boot
 {
-
     public static function load()
     {
+        if (!class_exists('ACF')) {
+            add_action('admin_notices', [self::class, 'no_acf_error_message']);
+            return;
+        }
+        
         self::load_timber();
         self::load_services();
         self::load_modules();
     }
 
+    public static function no_acf_error_message()
+    {
+        echo '<div class="notice notice-error">
+		<p>This theme require ACF Pro installed to work.</p>
+	    </div>';
+    }
+    
     public static function load_timber()
     {
         Timber::init();
@@ -33,6 +43,4 @@ class Boot
             new $class();
         }
     }
-
-
 }
